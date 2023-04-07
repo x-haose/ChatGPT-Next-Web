081381b1739a8c329626c33810db091312164026
昊色居士 <xhrtxh@gmail.com>
2023-04-07 22:33:29 +0800

    获取提示语脚本增加中文繁体语言

--- scripts/fetch-prompts.mjs
+++ scripts/fetch-prompts.mjs
@@ -5,10 +5,17 @@ const RAW_CN_URL =
   "https://raw.githubusercontent.com/PlexPt/awesome-chatgpt-prompts-zh/main/prompts-zh.json";
 const CN_URL =
   "https://cdn.jsdelivr.net/gh/PlexPt/awesome-chatgpt-prompts-zh@main/prompts-zh.json";
+
+const RAW_CN_TW_URL =
+  "https://raw.githubusercontent.com/PlexPt/awesome-chatgpt-prompts-zh/main/prompts-zh-TW.json";
+const CN_TW_URL =
+  "https://cdn.jsdelivr.net/gh/PlexPt/awesome-chatgpt-prompts-zh@main/prompts-zh-TW.json";
+
 const RAW_EN_URL =
   "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv";
 const EN_URL =
   "https://cdn.jsdelivr.net/gh/f/awesome-chatgpt-prompts@main/prompts.csv";
+
 const FILE = "./public/prompts.json";
 
 async function fetchCN() {
@@ -18,6 +25,17 @@ async function fetchCN() {
     return raw.map((v) => [v.act, v.prompt]);
   } catch (error) {
     console.error("[Fetch] failed to fetch cn prompts", error);
+    return [];
+  }
+}
+
+async function fetchTW() {
+  console.log("[Fetch] fetching cn-tw prompts...");
+  try {
+    const raw = await (await fetch(CN_TW_URL)).json();
+    return raw.map((v) => [v.act, v.prompt]);
+  } catch (error) {
+    console.error("[Fetch] failed to fetch cn-tw prompts", error);
     return [];
   }
 }
@@ -37,13 +55,13 @@ async function fetchEN() {
 }
 
 async function main() {
-  Promise.all([fetchCN(), fetchEN()])
-    .then(([cn, en]) => {
-      fs.writeFile(FILE, JSON.stringify({ cn, en }));
+  Promise.all([fetchCN(), fetchTW(), fetchEN()])
+    .then(([cn, en, tw]) => {
+      fs.writeFile(FILE, JSON.stringify({ cn, en, tw }));
     })
     .catch((e) => {
       console.error("[Fetch] failed to fetch prompts");
-      fs.writeFile(FILE, JSON.stringify({ cn: [], en: [] }));
+      fs.writeFile(FILE, JSON.stringify({ cn: [], en: [], tw: [] }));
     })
     .finally(() => {
       console.log("[Fetch] saved to " + FILE);
